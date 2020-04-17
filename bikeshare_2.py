e71fe1afe9dd68a20c420a021c81138ba7b2b724
Simon Bogner <simon.bogner88@gmail.com>
2020-04-17 16:46:29 +0200

    Comment out the package datetime

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -1,7 +1,7 @@
 import time as time
 import pandas as pd
 #import numpy as np
-import datetime as dt
+#import datetime as dt
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
